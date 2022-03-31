42e675da8350ecd2c528310e48433bab2df31ef2
pradumansharma9211 <71625116+pradumansharma9211@users.noreply.github.com>
2022-03-31 13:17:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import New from './component/New';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <New/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/New.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function New() {
+    return (
+        <div>
+            <div id="root">
+                <p>I am learning React. My life is getting better.</p>
+            </div>
+        </div>
+    )
+}
